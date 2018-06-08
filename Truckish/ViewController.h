fc0f48a7ee7acb8d7f2370481b7d0a33df4fbdca
Abhijit Paul <abhijit@elyxr.com>
2018-06-08 15:21:26 +0530

    added initial files

---
+++ Truckish/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Truckish
+//
+//  Created by Abhijit Paul on 08/06/18.
+//  Copyright © 2018 Abhijit Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
